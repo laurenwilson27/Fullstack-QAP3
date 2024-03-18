40704aa6420653d2e7d142987695bf3f83c7d807
laurenwilson27 <140430039+laurenwilson27@users.noreply.github.com>
2024-03-18 19:23:55 -0230

    Basic express functionality

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const PORT = 3000;
+
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+
+// Use the 'static' middleware to map the 'public' directory's static files to a route
+app.use("/resources", express.static("public"));
+
+// Index page. By default, all views to be handled by the view engine are inside the 'views' directory
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+// Listen!
+app.listen(PORT, () => {
+  console.log(
+    `Web server listening on port ${PORT} (http://localhost:${PORT})`
+  );
+});
